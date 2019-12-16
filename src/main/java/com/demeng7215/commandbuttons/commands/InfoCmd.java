4bc0d0b5458b6c6391cdc2096ca332fd3ffc87b3
Demeng <Demeng7215Official@gmail.com>
2019-12-16 17:04:36 -0500

    Fix info message and dev notifications

--- src/main/java/com/demeng7215/commandbuttons/commands/InfoCmd.java
+++ src/main/java/com/demeng7215/commandbuttons/commands/InfoCmd.java
@@ -18,7 +18,7 @@ public class InfoCmd extends CustomCommand {
 
 	@Override
 	protected void run(CommandSender sender, String[] args) {
-		MessageUtils.tellWithoutPrefix(sender, "&6Running CommandButtons v" + Common.getVersion() +
+		MessageUtils.tellWithoutPrefix(sender, "&4Running CommandButtons v" + Common.getVersion() +
 						" by Demeng7215.",
 				"&7https://spigotmc.org/resources/60991/",
 				"&6Type &7/cbhelp &6for a list of commands.");
