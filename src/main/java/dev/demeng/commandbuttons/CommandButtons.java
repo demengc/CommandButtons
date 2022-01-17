cb8590b6dd9177068618ca3ef0f76b93c8b98982
Demeng7215 <demengc6@gmail.com>
2022-01-17 17:39:10 -0500

    Initial commit for v5

---
+++ src/main/java/dev/demeng/commandbuttons/CommandButtons.java
@@ -0,0 +1,16 @@
+package dev.demeng.commandbuttons;
+
+import dev.demeng.pluginbase.plugin.BasePlugin;
+
+public final class CommandButtons extends BasePlugin {
+
+  @Override
+  public void enable() {
+
+  }
+
+  @Override
+  public void disable() {
+
+  }
+}
